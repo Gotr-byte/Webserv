c2ad5373a5fded167731a31feaa35b5814fcd216
Piotr Biedermann <piotr.biedermann84@gmail.com>
2023-05-06 13:30:21 +0200

    listening from two listening sockets

--- main.cpp
+++ main.cpp
@@ -22,7 +22,9 @@
 
 static int handleRequest(int port, int etc_port) {
 
-    etc_port = 0;
+    //for single socket
+    // etc_port = 0;
+
     // Create a socket for the server
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
@@ -58,48 +60,48 @@ static int handleRequest(int port, int etc_port) {
         exit(EXIT_FAILURE);
     }
 
-//     int etc_fd = create_socket(etc_port, &server_addr);
+    // int etc_fd = create_socket(etc_port, &server_addr);
 
-//    int etc_fd = socket(AF_INET, SOCK_STREAM, 0);
-//     if (etc_fd < 0) {
-//         perror("Error creating server socket");
-//         exit(EXIT_FAILURE);
-//     }
+   int etc_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (etc_fd < 0) {
+        perror("Error creating server socket");
+        exit(EXIT_FAILURE);
+    }
 
-//     //set the socket to non blocking
-//     fcntl(etc_fd, F_SETFL, O_NONBLOCK);
+    //set the socket to non blocking
+    fcntl(etc_fd, F_SETFL, O_NONBLOCK);
 
-//     // Set the server socket options
-//     // int opt = 1;
-//     if (setsockopt(etc_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-//         perror("Error setting server socket options");
-//         exit(EXIT_FAILURE);
-//     }
+    // Set the server socket options
+    int etc_opt = 2;
+    if (setsockopt(etc_fd, SOL_SOCKET, SO_REUSEADDR, &etc_opt, sizeof(etc_opt))) {
+        perror("Error setting server socket options");
+        exit(EXIT_FAILURE);
+    }
 
-//     // Bind the server socket to a specific port
-//     struct sockaddr_in server_addr_etc;
-//     memset(&server_addr, 0, sizeof(server_addr));
-//     server_addr_etc.sin_family = AF_INET;
-//     server_addr_etc.sin_addr.s_addr = INADDR_ANY;
-//     server_addr_etc.sin_port = htons(etc_port);
-//     if (bind(etc_fd, (struct sockaddr *)&server_addr_etc, sizeof(server_addr_etc)) < 0) {
-//         perror("Error binding server socket etc");
-//         exit(EXIT_FAILURE);
-//     }
+    // Bind the server socket to a specific port
+    struct sockaddr_in server_addr_etc;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr_etc.sin_family = AF_INET;
+    server_addr_etc.sin_addr.s_addr = INADDR_ANY;
+    server_addr_etc.sin_port = htons(etc_port);
+    if (bind(etc_fd, (struct sockaddr *)&server_addr_etc, sizeof(server_addr_etc)) < 0) {
+        perror("Error binding server socket etc");
+        exit(EXIT_FAILURE);
+    }
 
-//     // Listen for incoming connections on the server socket
-//     if (listen(etc_fd, MAX_CLIENTS) < 0) {
-//         perror("Error listening on server socket");
-//         exit(EXIT_FAILURE);
-//     }
+    // Listen for incoming connections on the server socket
+    if (listen(etc_fd, MAX_CLIENTS) < 0) {
+        perror("Error listening on server socket");
+        exit(EXIT_FAILURE);
+    }
 
     // Initialize the pollfd struct for the server socket
     struct pollfd fds[MAX_CLIENTS];
     memset(fds, 0, sizeof(fds));
     fds[0].fd = server_fd;
     fds[0].events = POLLIN;
-    // fds[1].fd = etc_fd;
-    // fds[1].events = POLLIN;
+    fds[1].fd = etc_fd;
+    fds[1].events = POLLIN;
 
     // Initialize the list of connected client sockets
     std::vector<int> clients;
@@ -109,7 +111,7 @@ static int handleRequest(int port, int etc_port) {
     while (true) {
 
         // Wait for events on any of the sockets
-        int nfds = clients.size() + 1;
+        int nfds = clients.size() + 2;
         int res = poll(fds, nfds, -1);
         if (res < 0) {
             perror("Error polling sockets");
@@ -132,39 +134,39 @@ static int handleRequest(int port, int etc_port) {
             clients.push_back(client_fd);
 
             // Initialize the pollfd struct for the client socket
-            fds[clients.size()].fd = client_fd;
-            fds[clients.size()].events = POLLIN;
-            fds[clients.size()].revents = 0;
+            fds[clients.size() + 1].fd = client_fd;
+            fds[clients.size() + 1].events = POLLIN;
+            fds[clients.size() + 1].revents = 0;
 
             std::cout << "New client connected on server fd\n";
         }
-        // if (fds[1].revents & POLLIN) {
+        if (fds[1].revents & POLLIN) {
 
-        //     // Accept a new connection from a client
-        //     struct sockaddr_in client_addr;
-        //     socklen_t client_len = sizeof(client_addr);
-        //     int client_fd = accept(etc_fd, (struct sockaddr *)&client_addr, &client_len);
-        //     if (client_fd < 0) {
-        //         perror("Error accepting client connection on etc");
-        //         exit(EXIT_FAILURE);
-        //     }
+            // Accept a new connection from a client
+            struct sockaddr_in client_addr;
+            socklen_t client_len = sizeof(client_addr);
+            int client_fd = accept(etc_fd, (struct sockaddr *)&client_addr, &client_len);
+            if (client_fd < 0) {
+                perror("Error accepting client connection on etc");
+                exit(EXIT_FAILURE);
+            }
 
-        //     // Add the client socket to the list of connected clients
-        //     clients.push_back(client_fd);
+            // Add the client socket to the list of connected clients
+            clients.push_back(client_fd);
 
-        //     // Initialize the pollfd struct for the client socket
-        //     fds[clients.size()].fd = client_fd;
-        //     fds[clients.size()].events = POLLIN;
-        //     fds[clients.size()].revents = 0;
+            // Initialize the pollfd struct for the client socket
+            fds[clients.size() + 1].fd = client_fd;
+            fds[clients.size() + 1].events = POLLIN;
+            fds[clients.size() + 1].revents = 0;
 
-        //     std::cout << "New client connected on etc fd\n";
-        // }
+            std::cout << "New client connected on etc fd\n";
+        }
 
         // Check for events on any of the connected client sockets
         for (unsigned long i = 0; i < clients.size(); i++) 
         {
             // printf("i equals %lu\n",i);
-            if (fds[i + 1].revents & POLLIN) {
+            if (fds[i + 2].revents & POLLIN) {
 
                 // Receive data from the client
                 char buf[BUF_SIZE];
