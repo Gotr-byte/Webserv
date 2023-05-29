39be092adb36225e986d339247217257ad3d7b5e
bomsi39 <damfranke@gmail.com>
2023-05-29 17:31:51 +0200

    compatibility changes

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -2,7 +2,7 @@
 
 class Client {
     public:
-        Client():fd(-1), initialResponseSent(false), content_length(0), file_fd(-1){};
+        Client():fd(-1), initialResponseSent(false), file_fd(-1), content_length(0){};
         int fd;                      // Client file descriptor
         bool initialResponseSent;    // Flag indicating if initial response headers have been sent
         int file_fd;                 // File descriptor for the requested file
