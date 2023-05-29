90401be72f3d4fc8d1c05c572a06605ebf31d2b2
piotr biedermann <piotr.biedermann84@gmail.com>
2023-05-29 15:41:07 +0200

    added files

---
+++ includes/Client.hpp
@@ -0,0 +1,8 @@
+class Client {
+    public:
+        Client():fd(-1), initialResponseSent(false), content_length(0){};
+        int fd;                      // Client file descriptor
+        bool initialResponseSent;    // Flag indicating if initial response headers have been sent
+        int file_fd;                 // File descriptor for the requested file
+        off_t content_length;        // Content length of the requested file
+};
