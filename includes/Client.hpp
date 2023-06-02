279c98ecb0b80fe249c74b4f8dd0ffa464dd3029
piotr biedermann <piotr.biedermann84@gmail.com>
2023-06-02 14:08:18 +0200

    created socket class for refactoring

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -7,4 +7,5 @@ class Client {
         bool initialResponseSent;    // Flag indicating if initial response headers have been sent
         int file_fd;                 // File descriptor for the requested file
         off_t content_length;        // Content length of the requested file
+        
 };
