eb7c393f1ebc3e1a9baf3b9199bd14e3aec1ce7d
piotr biedermann <piotr.biedermann84@gmail.com>
2023-06-10 18:08:16 +0200

    cgi runs

---
+++ HTML/cgi-bin/print_params.py
@@ -0,0 +1,11 @@
+import cgi
+# Create a FieldStorage object to access the parameters
+form = cgi.FieldStorage()
+
+# Get the values of the parameters
+param1 = form.getvalue('param1')
+param2 = form.getvalue('param2')
+
+# Print the parameter values
+print("Parameter 1:", param1)
+print("Parameter 2:", param2)
