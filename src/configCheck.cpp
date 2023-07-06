4a89fd44985721e683816d5fa826407baf62dcb3
piotr biedermann <piotr.biedermann84@gmail.com>
2023-07-06 09:29:45 +0200

    added config file checkks to checker

--- src/configCheck.cpp
+++ src/configCheck.cpp
@@ -1,5 +1,20 @@
 #include "../includes/ConfigCheck.hpp"
 
+static void checkFileExtension(const std::string& filename) {
+    if (filename.length() < 7 || filename.substr(filename.length() - 7) != ".config") {
+        std::cerr << "Error: Invalid file extension. Expected '.config'" << std::endl;
+        exit(1);
+    }
+}
+
+static void checkFileEmpty(const std::string& filename) {
+    std::ifstream file(filename.c_str());
+    if (file.peek() == std::ifstream::traits_type::eof()) {
+        std::cerr << "Error: File is empty." << std::endl;
+        std::exit(1);
+    }
+}
+
 static void	checkLocationBlock(std::fstream &config, std::string &line)
 {
 	while (line.find("</location>") == std::string::npos)
@@ -68,6 +83,8 @@ int	ConfigCheck::checkConfig(std::string path)
 	std::string		line;
 	int				server_counter = 0;
 
+	checkFileExtension(path);
+	checkFileEmpty(path);
 	config.open(path.c_str(), std::fstream::in);
 
 	while (!getline(config, line).eof())
